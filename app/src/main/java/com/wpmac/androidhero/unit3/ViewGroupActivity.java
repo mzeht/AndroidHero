9cb700692f89662ba75baff7c180bfb27b570c72
mzeht <m18989498303@163.com>
2016-11-10 13:46:30 +0800

    ViewGroup TouchEvent

---
+++ app/src/main/java/com/wpmac/androidhero/unit3/ViewGroupActivity.java
@@ -0,0 +1,15 @@
+package com.wpmac.androidhero.unit3;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.wpmac.androidhero.R;
+
+public class ViewGroupActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_group);
+    }
+}
